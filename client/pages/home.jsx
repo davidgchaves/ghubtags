839a00698b55d0a3f02947ad2f33ce908dc48c64
David G Chaves <davidgchaves@gmail.com>
2016-01-30 13:55:14 +0100

    Better code formatting

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -4,15 +4,18 @@ import NavHelper from '../components/nav-helper'
 
 const Home = () => (
   <NavHelper className='container'>
+
     <header role='banner'>
       <h1>gHubTags</h1>
     </header>
+
     <div>
       <p>We label stuff for you, because, we can&trade;</p>
       <a href='/login' className='button button-large'>
         <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
       </a>
     </div>
+
   </NavHelper>
 );
 
