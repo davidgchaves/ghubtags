b1b1c25e7ea28e883e7b235703b48d4380f0e3bf
David G Chaves <davidgchaves@gmail.com>
2016-02-03 20:45:43 +0100

    Add edit and show label templates (with colors!)

--- client/components/label-item.jsx
+++ client/components/label-item.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const LabelItem = ({ label }) => (
-  <li>{label.name}</li>
-);
+const LabelItem = ({ label }) => {
+  const cssColor = '#' + label.color;
+
+  const editingLabel = (
+    <div>
+      <form className='label'>
+        <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+        <input name='name'/>
+        <input name='color'/>
+        <button type='submit' className='button button-small'>Save</button>
+        <button type='button' className='button button-small button-unstyled'>cancel</button>
+      </form>
+    </div>
+  );
+
+  const showLabel = (
+    <div>
+      <div className='label'>
+        <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
+        <span>{label.name}</span>
+        <span className='octicon octicon-pencil'></span>
+        <span className='octicon octicon-x'></span>
+      </div>
+    </div>
+  );
+
+  return label.editing ? editLabel : showLabel;
+};
 
 export default LabelItem;
