c29878f6e16ed84be889fcbbbad27c1a526a47e9
David G Chaves <davidgchaves@gmail.com>
2016-02-04 09:16:39 +0100

    Show the edit label template when clicked on edit label

--- client/components/label-item.jsx
+++ client/components/label-item.jsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LabelItem = ({ label }) => {
-  const cssColor = '#' + label.color;
+import ampersandMixin from 'ampersand-react-mixin';
 
-  const editLabel = (
-    <div>
-      <form className='label'>
-        <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
-        <input name='name'/>
-        <input name='color'/>
-        <button type='submit' className='button button-small'>Save</button>
-        <button type='button' className='button button-small button-unstyled'>cancel</button>
-      </form>
-    </div>
-  );
+export default React.createClass({
+  mixins: [ampersandMixin],
 
-  const showLabel = (
-    <div>
-      <div className='label'>
-        <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
-        <span>{label.name}</span>
-        <span className='octicon octicon-pencil'></span>
-        <span className='octicon octicon-x'></span>
+  displayName: 'LabelItem',
+
+  onEditClick () {
+    this.props.label.editing = true;
+  },
+
+  render () {
+    const { label } = this.props;
+    const cssColor = '#' + label.color;
+
+    const editLabel = (
+      <div>
+        <form className='label'>
+          <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+          <input name='name'/>
+          <input name='color'/>
+          <button type='submit' className='button button-small'>Save</button>
+          <button type='button' className='button button-small button-unstyled'>cancel</button>
+        </form>
       </div>
-    </div>
-  );
+    );
 
-  return label.editing ? editLabel : showLabel;
-};
+    const showLabel = (
+      <div>
+        <div className='label'>
+          <span className='label-color' style={{backgroundColor: cssColor}}>&nbsp;</span>
+          <span>{label.name}</span>
+          <span onClick={this.onEditClick} className='octicon octicon-pencil'></span>
+          <span className='octicon octicon-x'></span>
+        </div>
+      </div>
+    );
 
-export default LabelItem;
+    return label.editing ? editLabel : showLabel;
+  }
+});
