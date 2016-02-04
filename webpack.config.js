aeb821fd960c7a7a981be6ffe1d9559121fa7f2d
David G Chaves <davidgchaves@gmail.com>
2016-02-04 19:07:23 +0100

    Pre-render Home Page on server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-var getConfig = require('hjs-webpack');
+require('babel-register');
+var getConfig      = require('hjs-webpack');
+var React          = require('react');
+var ReactDOMServer = require('react-dom/server');
+var HomePage       = require('./client/pages/home').default;
 
 module.exports = getConfig({
   in: 'client/app.js',
   out: 'public',
   clearBeforeBuild: true,
-  html: function (context) {
+  html: function (data) {
+    const homePageHtmlString =
+      ReactDOMServer.renderToString(React.createElement(HomePage));
     return {
-      'index.html': context.defaultTemplate(),
-      '200.html': context.defaultTemplate()
+      'index.html': data.defaultTemplate({html: homePageHtmlString}),
+      '200.html': data.defaultTemplate()
     };
   }
 });
