a8f381aef432823848da7021b269e63274c61655
David G Chaves <davidgchaves@gmail.com>
2016-02-04 19:20:00 +0100

    Pre-render Layout Page on server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var getConfig      = require('hjs-webpack');
 var React          = require('react');
 var ReactDOMServer = require('react-dom/server');
 var HomePage       = require('./client/pages/home').default;
+var LayoutPage     = require('./client/layout').default;
 
 module.exports = getConfig({
   in: 'client/app.js',
@@ -11,9 +12,12 @@ module.exports = getConfig({
   html: function (data) {
     const homePageHtmlString =
       ReactDOMServer.renderToString(React.createElement(HomePage));
+    const layoutPageHtmlString =
+      ReactDOMServer.renderToString(React.createElement(LayoutPage, {me: {}}));
+
     return {
       'index.html': data.defaultTemplate({html: homePageHtmlString}),
-      '200.html': data.defaultTemplate()
+      '200.html': data.defaultTemplate({html: layoutPageHtmlString})
     };
   }
 });
